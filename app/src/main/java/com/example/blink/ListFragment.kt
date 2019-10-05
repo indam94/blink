35f9c3b3196c80e80cc0ca565c3338c18833ccc9
minoring <minho1350@gmail.com>
2019-10-05 15:28:46 -0700

    Remove redundant files

--- app/src/main/java/com/example/blink/ListFragment.kt
+++ app/src/main/java/com/example/blink/ListFragment.kt
@@ -1,23 +1,45 @@
 package com.example.blink
 
-
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 
-/**
- * A simple [Fragment] subclass.
- */
 class ListFragment : Fragment() {
+    companion object {
+        fun newInstance(message: String): ListFragment {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+            val f = ListFragment()
+
+            val bdl = Bundle(1)
+
+            bdl.putString(EXTRA_MESSAGE, message)
+
+            f.setArguments(bdl)
+
+            return f
+
+        }
+    }
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        var view: View? = inflater.inflate(R.layout.fragment_home, container, false);
+
+        val message = arguments!!.getString(EXTRA_MESSAGE)
+
+        var textView: TextView = view!!.findViewById(R.id.text)
+        textView!!.text = message
+
+        return view
     }
 
 
