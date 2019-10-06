3fe6e16587e049b6fd141ed9c93ef81767d49d3e
minoring <minho1350@gmail.com>
2019-10-05 17:04:06 -0700

    Add FAB button

--- app/src/main/java/com/example/blink/SharingActivity.kt
+++ app/src/main/java/com/example/blink/SharingActivity.kt
@@ -13,7 +13,7 @@ class SharingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_sharing)
 
         initViews()
 
